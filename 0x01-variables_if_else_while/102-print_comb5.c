077d20bd2e16eac9f8a8087054e90f66336d100c
Gladysgala <yatorgladys2@gmail.com>
2022-12-12 11:48:36 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,32 +3,31 @@
 #include <time.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
  * Return: Always 0
- */
+*/
 
 int main(void)
-{
-	int i, j;
+{int i, j;
 
-	for (i = 0; i <= 98; i++)
+	for (i = 0; i <= 99; i++)
 	{
 		for (j = 0; j <= 99; j++)
-
 		{
-			putchar((i / 10) + '0');
-			putchar((i / 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			
-			if  (i == 98 && j == 99)
-				continue;
-
-				putchar(',');
+			if (i < j && i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-			
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
