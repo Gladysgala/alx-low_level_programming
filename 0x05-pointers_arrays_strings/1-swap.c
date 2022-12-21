ec5b32ba9df1cef2070b10c5dc5b9d692cc27ca0
Gladysgala <yatorgladys2@gmail.com>
2022-12-21 13:17:40 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 1
+		b = 2
+		printf("a=%d, b=%d\n", a, b);
+	swap_int($a, $b);
+	print ("a=%d, b=%d\n", a, b);
+	return (0);
+}
