51b63a4d08f57a784127474d599c8e46c2daf1bd
Gladysgala <yatorgladys2@gmail.com>
2023-08-15 00:36:16 -0700

    part-a

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - Custom function to output a character
+ * @c: The character to be output
+ *
+ * Return: The character that was output
+ */
+int _putchar(char c)
+{
+	putchar(c);
+	return (c);
+}
+
