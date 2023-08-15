6bf9083d1997e225c21c564c4c40fcce36daaea1
Gladysgala <yatorgladys2@gmail.com>
2023-08-15 02:02:05 -0700

    partc

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isdigit - check if it is a digit
+ *  @c: the character that needs checking
+ *
+ *  Return: 1 if c is digit and 0 if not
+ */
+
+int _isdigit(int c)
+{
+return (isdigit(c));
+}
